cfbb30efa211bcfa13e31d9b0e06b7ebb901be44
Monticorik <78502350+Monticorik@users.noreply.github.com>
2024-03-12 19:01:39 +0200

    Update apolloClient.ts

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/',
+  uri: 'https://pharmacy-shop-app-server-chi.vercel.app/',
 });
 
 const client = new ApolloClient({
